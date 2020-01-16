8c82580267df367bd3e37aaffd59ff3245bacc7b
[Principe Cabrera] <[pjcabrera0@gmail.com]>
2020-01-15 23:53:02 -0400

    imported shows and added middleware for shows routes

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 
 const genres = require(`./routes/genres`);
 const customers = require(`./routes/customers`);
+const shows = require(`./routes/shows`);
+// console.log('here')
 const port = process.env.PORT || 3001;
 
 
 app.use(`/api/genres/`, genres);
 app.use(`/api/customers/`, customers);
+app.use(`/api/shows/`, shows);
 app.get(`/`, (req, res) => {res.send(`OOPS! SOMETHING'S DEFINITELY WRONG!`)});
 app.listen(port, () => console.log(`Listening on port ${port}`));
